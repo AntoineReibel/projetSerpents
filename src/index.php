8dffa60af514668d0724f953f16c03b60124245c
Antoine <antoine.reib3l@gmail.com>
2023-10-22 22:17:24 +0200

    delete, chemin relatif pour le output

--- src/index.php
+++ src/index.php
@@ -13,7 +13,7 @@ include_once ('./function/datesFormatees.php');
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="/dist/output.css" rel="stylesheet">
+    <link href="../dist/output.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Lato:wght@400;700;900&display=swap"
--- src/page/delete.php
+++ src/page/delete.php
@@ -1,1 +1,29 @@
 <?php
+
+use class\Serpents;
+
+$serpent = new Serpents($_GET['id']);
+if (isset($_POST['kill'])){
+    $serpent->delete();
+    header('Location: index.php?page=vivarium');
+}
+
+
+?>
+
+<p>Allez-vous vraiment tuer <?php echo $serpent->get('isMale') ? " le " : " la " ?>
+    pauvre <?= $serpent->get('nomSerpent') ?> ?</p>
+<div class="flex gap-2 mt-2">
+    <form action="" method="post">
+        <button name='kill'
+                class="cursor-pointer text-white font-bold shadow-md hover:scale-[1.2] shadow-purple-400 rounded-full px-5 py-2 bg-gradient-to-bl from-red-500 to-red-600">
+            Oui
+        </button>
+    </form>
+
+    <a href="index.php?page=vivarium">
+        <button class="cursor-pointer text-white font-bold shadow-md hover:scale-[1.2] shadow-purple-400 rounded-full px-5 py-2 bg-gradient-to-bl from-indigo-500 to-blue-500">
+            Non
+        </button>
+    </a>
+</div>
