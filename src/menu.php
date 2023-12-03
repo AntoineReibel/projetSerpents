d0c641414e5859254bf84c26da3d64120cd1d3bf
Antoine <antoine.reib3l@gmail.com>
2023-12-03 19:11:00 +0100

    changement du lien principal

--- src/menu.php
+++ src/menu.php
@@ -1,7 +1,7 @@
 <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-emerald-500 mb-4">
     <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-            <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="index.php">
+            <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="index.php?page=vivarium">
                 Serpentarium
             </a>
             <button class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
